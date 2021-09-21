556af613b7159640085ea6ea895af3efc297f970
Amaya Agha <amaya.agha@gmail.com>
2021-09-20 21:51:11 -0400

    tests(redux/products): adds tests for products redux

---
+++ client/constants.js
@@ -0,0 +1,12 @@
+/*************************
+ * Fetch States          *
+ ************************/
+const FETCH_PENDING = 'FETCH_PENDING';
+const FETCH_SUCCESS = 'FETCH_SUCCESS';
+const FETCH_FAILED = 'FETCH_FAILURE';
+
+module.exports = {
+  FETCH_PENDING,
+  FETCH_SUCCESS,
+  FETCH_FAILED
+}
