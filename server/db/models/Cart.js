caaa251e13fff73e79cc7ce742b13ff158db8a24
Alexandra Marks <amarks93@gmail.com>
2021-09-23 13:02:12 -0400

    chore: add status field to cart model

--- server/db/models/Cart.js
+++ server/db/models/Cart.js
@@ -1,16 +1,20 @@
-const Sequelize = require('sequelize')
-const db = require('../db')
+const Sequelize = require("sequelize");
+const db = require("../db");
 
-
-const Cart = db.define('cart', {
+const Cart = db.define("cart", {
   quantity: {
     type: Sequelize.INTEGER,
     allowNull: false,
     defaultValue: 1,
     validate: {
-      min: 1,
+      min: 1
     }
+  },
+  status: {
+    type: Sequelize.ENUM("PENDING", "FULFILLED"),
+    allowNull: false,
+    defaultValue: "PENDING"
   }
-})
+});
 
-module.exports = Cart
+module.exports = Cart;
