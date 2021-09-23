f4817b5604e7351c06eb6c4166e069e0f8f784e8
Alexandra Marks <amarks93@gmail.com>
2021-09-23 09:07:42 -0400

    chore: move styling to mui from bootstrap for allproducts, productrow, navbar

--- client/components/ProductRow.js
+++ client/components/ProductRow.js
@@ -4,9 +4,60 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addToCartThunk } from "../store/cart";
 
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    margin: "10px",
+    marginBottom: "40px",
+    height: "280px",
+    width: "200px",
+    border: "8px solid #fcd000",
+    borderRadius: "10px",
+    "&:hover": {
+      boxShadow: "0 0 10px 5px #cccccc",
+      transition: "all .4s ease"
+    }
+  },
+  cardContent: {
+    padding: "5px",
+    display: "flex",
+    flexDirection: "column",
+    height: "100%",
+    paddingBottom: "20px"
+  },
+  cardImage: {
+    borderRadius: "2px",
+    border: "3px solid #fcd000"
+  },
+  button: {
+    backgroundColor: "#fcd000",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "#e71e07",
+      transition: "all .4s ease"
+    },
+    marginTop: "auto"
+  },
+  cardBody: {
+    display: "flex",
+    flexGrow: "1",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-between"
+  }
+}));
+
 const ProductRow = (props) => {
   const { product } = props;
   const { id, name, description, price, imageUrl } = product;
+
+  const styles = useStyles();
 
   const dispatch = useDispatch();
 
@@ -15,21 +66,43 @@ const ProductRow = (props) => {
     dispatch(addToCartThunk(productId));
   };
   return (
-    <div className="col-lg-2 col-md-3 col-sm-4 card p-4 m-2">
-      <Link className="product-link" to={`/products/${id}`}>
-        <h5 className="card-title">{name}</h5>
-      </Link>
+    <Card className={styles.card}>
       <Link to={`/products/${id}`}>
-        <img className="card-img" src={imageUrl} alt="Card image cap" />
+        <CardContent className={styles.cardContent}>
+          <Typography sx={{ fontSize: 10 }}>{name}</Typography>
+          <div className={styles.cardBody}>
+            <CardMedia
+              className={styles.cardImage}
+              component="img"
+              height="120"
+              image={imageUrl}
+              alt={name}
+            />
+            <Typography sx={{ mb: 1.5 }}>${price}</Typography>
+            <Button variant="contained" value={id} onClick={handleClick} className={styles.button}>
+              ADD TO CART
+            </Button>
+          </div>
+        </CardContent>
       </Link>
-      <div className="card-body">
-        <p className="card-text">$ {price}</p>
+    </Card>
 
-        <button value={id} onClick={handleClick} className="btn btn-secondary add-to-cart">
-          ADD TO CART
-        </button>
-      </div>
-    </div>
+    // <div>
+    //   <div>
+    //     <Link className="product-link" to={`/products/${id}`}>
+    //       <h5 className="">{name}</h5>
+    //     </Link>
+    //     <img className="card-img" src={imageUrl} alt="Card image cap" />
+
+    //     <div className="">
+    //       <p className="">$ {price}</p>
+
+    //       <button value={id} onClick={handleClick} className="add-to-cart">
+    //         ADD TO CART
+    //       </button>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
