d84014bb334ec36f054971795cb004d6c0d039a3
Alexandra Marks <amarks93@gmail.com>
2021-09-23 09:37:06 -0400

    refactor: change button target to currenttarget

--- client/components/ProductRow.js
+++ client/components/ProductRow.js
@@ -22,18 +22,20 @@ const useStyles = makeStyles((theme) => ({
     "&:hover": {
       boxShadow: "0 0 10px 5px #cccccc",
       transition: "all .4s ease"
-    }
+    },
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#FFF3E0"
   },
   cardContent: {
     padding: "5px",
     display: "flex",
     flexDirection: "column",
-    height: "100%",
-    paddingBottom: "20px"
+    height: "70%"
   },
   cardImage: {
     borderRadius: "2px",
-    border: "3px solid #fcd000"
+    border: "5px solid #fcd000"
   },
   button: {
     backgroundColor: "#fcd000",
@@ -42,7 +44,9 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: "#e71e07",
       transition: "all .4s ease"
     },
-    marginTop: "auto"
+    margin: "5x",
+    width: "150px",
+    alignSelf: "center"
   },
   cardBody: {
     display: "flex",
@@ -50,6 +54,21 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "space-between"
+  },
+  cardPrice: {
+    marginTop: "20px",
+    color: "#484848",
+    "&:hover": {
+      color: "#616161"
+    }
+  },
+  cardTitle: {
+    color: "#484848",
+    "&:hover": {
+      color: "#616161"
+    },
+    marginLeft: "2px",
+    fontWeight: "bold"
   }
 }));
 
@@ -62,14 +81,17 @@ const ProductRow = (props) => {
   const dispatch = useDispatch();
 
   const handleClick = (evt) => {
-    const productId = evt.target.value;
+    console.log(evt.currentTarget.value);
+    const productId = evt.currentTarget.value;
     dispatch(addToCartThunk(productId));
   };
   return (
     <Card className={styles.card}>
       <Link to={`/products/${id}`}>
         <CardContent className={styles.cardContent}>
-          <Typography sx={{ fontSize: 10 }}>{name}</Typography>
+          <Typography sx={{ fontSize: 10 }} className={styles.cardTitle}>
+            {name}
+          </Typography>
           <div className={styles.cardBody}>
             <CardMedia
               className={styles.cardImage}
@@ -78,31 +100,16 @@ const ProductRow = (props) => {
               image={imageUrl}
               alt={name}
             />
-            <Typography sx={{ mb: 1.5 }}>${price}</Typography>
-            <Button variant="contained" value={id} onClick={handleClick} className={styles.button}>
-              ADD TO CART
-            </Button>
+            <Typography sx={{ mb: 1.5 }} className={styles.cardPrice}>
+              ${price}
+            </Typography>
           </div>
         </CardContent>
       </Link>
+      <Button variant="contained" value={id} onClick={handleClick} className={styles.button}>
+        ADD TO CART
+      </Button>
     </Card>
-
-    // <div>
-    //   <div>
-    //     <Link className="product-link" to={`/products/${id}`}>
-    //       <h5 className="">{name}</h5>
-    //     </Link>
-    //     <img className="card-img" src={imageUrl} alt="Card image cap" />
-
-    //     <div className="">
-    //       <p className="">$ {price}</p>
-
-    //       <button value={id} onClick={handleClick} className="add-to-cart">
-    //         ADD TO CART
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
