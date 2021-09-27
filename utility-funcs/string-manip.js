5bcb3adc638883a02610ea58cb76d14fc7a9282c
Amaya Agha <amaya.agha@gmail.com>
2021-09-27 12:38:41 -0400

    fix: category filter now properly modifies url when multi-word category is removed

--- utility-funcs/string-manip.js
+++ utility-funcs/string-manip.js
@@ -1,12 +1,17 @@
 const snakeCase = (str) => {
-  return str.trim().toLowerCase().replace(' ', '_');
+  return str.trim().toLowerCase().replace(/\s+/g, '_');
 }
 
 const properCase = (str) => {
   return str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
 }
 
+const unSnakeCase = (str) => {
+  return str.trim().toLowerCase().replace(/_+/g, ' ');
+}
+
 module.exports = {
   snakeCase,
-  properCase
+  properCase,
+  unSnakeCase
 };
