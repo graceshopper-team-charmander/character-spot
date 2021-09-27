e917d6058cf974b2080697759a77bc4dfc6aecef
Alexandra Marks <amarks93@gmail.com>
2021-09-27 13:30:21 -0400

    qty check for single cart product

--- client/components/SingleCartProduct.js
+++ client/components/SingleCartProduct.js
@@ -9,7 +9,7 @@ import Typography from "@material-ui/core/Typography";
 import { useDispatch, useSelector } from "react-redux";
 import { updateQuantityThunk, deleteProductThunk } from "../store/cart";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import DeleteIcon from '@material-ui/icons/Delete';
+import DeleteIcon from "@material-ui/icons/Delete";
 import { deleteLocalCartProduct, updateLocalCartQuantity } from "../store/localCart";
 
 const useStyles = makeStyles((theme) => ({
@@ -26,13 +26,21 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+function checkQuantity(product) {
+  if (product.quantity - product.cartQuantity <= 0) {
+    alert(`There are only ${product.quantity} ${product.name}(s) left in stock`);
+    return false;
+  }
+  return true;
+}
+
 const SingleCartProduct = (props) => {
   const dispatch = useDispatch();
   const product = props.product;
   const muiClasses = useStyles();
-  const isLoggedIn = useSelector(state => state.auth.loggedIn);
-  const updateQuantity = (isLoggedIn ? updateQuantityThunk : updateLocalCartQuantity)
-  const deleteProduct = (isLoggedIn ? deleteProductThunk : deleteLocalCartProduct)
+  const isLoggedIn = useSelector((state) => state.auth.loggedIn);
+  const updateQuantity = isLoggedIn ? updateQuantityThunk : updateLocalCartQuantity;
+  const deleteProduct = isLoggedIn ? deleteProductThunk : deleteLocalCartProduct;
   return (
     <Card className={muiClasses.cardRoot}>
       <Box sx={{ display: "flex" }}>
@@ -46,28 +54,32 @@ const SingleCartProduct = (props) => {
           <Typography>{product.description}</Typography>
           <CardActions style={{ padding: "0px" }}>
             <div onClick={() => dispatch(updateQuantity(product, product.cartQuantity - 1))}>
-              <Button
-                className={muiClasses.buttonRoot}>
-                -
-              </Button>
+              <Button className={muiClasses.buttonRoot}>-</Button>
             </div>
             {product.cartQuantity}
-            <div onClick={() => dispatch(updateQuantity(product, product.cartQuantity + 1))}>
-              <Button
-                className={muiClasses.buttonRoot}>
-                +
-              </Button>
+            <div
+              onClick={() => {
+                if (checkQuantity(props.product)) {
+                  dispatch(updateQuantity(product, product.cartQuantity + 1));
+                }
+              }}>
+              <Button className={muiClasses.buttonRoot}>+</Button>
             </div>
             <div onClick={() => dispatch(deleteProduct(product))}>
-              <Button
-                className = {muiClasses.buttonRoot}>
-                  <DeleteIcon />
+              <Button className={muiClasses.buttonRoot}>
+                <DeleteIcon />
               </Button>
             </div>
           </CardActions>
         </Box>
         <Box sx={{ m: 2 }} style={{ flexGrow: 1 }}>
-          <Typography style={{ textAlign: "right" }}>Price: ${(product.price * product.cartQuantity / 100).toLocaleString('en', {'minimumFractionDigits':2,'maximumFractionDigits':2})}</Typography>
+          <Typography style={{ textAlign: "right" }}>
+            Price: $
+            {((product.price * product.cartQuantity) / 100).toLocaleString("en", {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2
+            })}
+          </Typography>
         </Box>
       </Box>
     </Card>
