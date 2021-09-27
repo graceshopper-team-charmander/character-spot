e80245c9cc16a3375dbd325bbd7ff7c814d3fc2c
Nicole <nicole.pan1127@gmail.com>
2021-09-26 20:12:44 -0400

    moved the oauth2 tokens

--- server/email/email.js
+++ server/email/email.js
@@ -36,10 +36,10 @@ async function sendConfirmEmail({to, html}) {
     auth: {
       type: 'OAuth2',
       user: "charactershop2108",
-      clientId: "231791445708-dk76p62lc7e1toveged39f2uq9h03k00.apps.googleusercontent.com",
-      clientSecret: "q3UKiU5xR2rPl22dZh-9aLSb",
-      refreshToken: "1//04y622reMlPNrCgYIARAAGAQSNwF-L9IrTr5RqxM-aV4JHFJdCKHC1hVOxsbum0E9iRYK8ox84uNXtWKCiz0XAlBNJfh5FbL-rxo",
-      accessToken: "ya29.a0ARrdaM-Xt5sddKViKpm1aY_C9GUW52ptaOJpwQ93u5J1hvWPk9qXfGPiuuierikyj0jZ0gikbk8LQonpw_Q11nTr4cbemgfLiWhRrrlq8Rs-IqAW7Rk2v0S-Rgei--TVzib_qnv3aPEJvcPXXYumroJnFqrw"
+      clientId: process.env.clientId,
+      clientSecret: process.env.clientSecret,
+      refreshToken: process.env.refreshToken,
+      accessToken: process.env.accessToken
   }
   });
   await transporter.sendMail({
