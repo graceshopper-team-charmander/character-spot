bfba54c40c1448b75a35d6c27cb05dd2c0f9e70d
Nicole <nicole.pan1127@gmail.com>
2021-09-28 17:32:56 -0400

    thankyou page styled

--- client/components/Thankyou.js
+++ client/components/Thankyou.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { submitGuestOrderThunk } from "../store/localCart";
 import { submitOrderThunk } from "../store/cart";
+import Box from "@material-ui/core/Box";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyles = makeStyles((theme) => ({
+  text: {
+    fontFamily: "mario",
+    textAlign: "center"
+  },
+  base: {
+    margin: "auto",
+    width: "75%",
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "ghostwhite"
+  },
+  cardRoot: {
+    margin: "20px",
+    justifyContent: "center"
+  }
+}));
 
 const Thankyou = (props) => {
+  const muiClasses = useStyles();
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart.cart);
   const {firstName, guestEmailAddress, lastName } = useSelector((state) => state.cart.form);
@@ -26,7 +47,17 @@ const Thankyou = (props) => {
   }, [fetchCartStatus]);
 
   return (
-    <div>Thanks for your order!!!!</div>
+    <Box className = {muiClasses.base}>
+      <Box className = {muiClasses.cardRoot}>
+      <div style = {{textAlign: "center"}}>
+      <h1 className={muiClasses.text}>Thank you for your order!</h1>
+      <img
+        src = "/images/charmander.png"
+        style = {{maxWidth: "50%", padding: "30px"}}/>
+      </div>
+      <h1 className={muiClasses.text}>From Team Charmander</h1>
+      </Box>
+  </Box>
   )
 
 };
