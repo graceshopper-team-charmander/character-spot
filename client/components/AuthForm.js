91666fb67c8429d30880d03d43872df26c28caf6
Alexandra Marks <amarks93@gmail.com>
2021-09-28 15:55:29 -0400

    test

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from "react";
 import { connect, useDispatch, useSelector } from "react-redux";
 import { authenticate } from "../store";
